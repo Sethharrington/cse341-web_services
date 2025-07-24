30608c5bfbdba5fcb2c30bcb150d8d72f8257454
Seth Harrington <sbgha.09@gmail.com>
2025-07-24 13:03:50 -0600

    Add CORS headers middleware for enhanced API access control

--- server.js
+++ server.js
@@ -18,6 +18,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 const corsOptions = { origin: "*" };
 app.use(cors(corsOptions));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 /* ***********************
