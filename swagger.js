493f654f9b246a940174107f271d87cb716036fe
Seth Harrington A <sbgha.09@gmail.com>
2025-07-26 16:45:54 -0700

    using prod doc

--- swagger.js
+++ swagger.js
@@ -5,8 +5,8 @@ const doc = {
     title: "My API",
     description: "Description",
   },
-  // host: "https://cse341-web-services-1qxq.onrender.com",
-  host: "localhost:8080", // Use localhost for local development
+  host: "https://cse341-web-services-1qxq.onrender.com",
+  // host: "localhost:8080", // Use localhost for local development
   schemes: ["http", "https"], // Use both https and http for production
 };
 
