f3912fa55b04679e2484ebb0d6cfe4aa645c780c
Seth Harrington A <sbgha.09@gmail.com>
2025-07-26 16:44:02 -0700

    fixing swagger

--- server.js
+++ server.js
@@ -33,19 +33,7 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 /* ***********************
  * Routes
  *************************/
-app.get("/", (req, res) => {
-  res.send(`
-    <h1>Welcome to the API</h1>
-    <p>Use the endpoints provided in the documentation to interact with the API.</p>
-    <ul>
-      <li><a href="/api-docs">API Documentation</a></li>
-      <li><a href="/professional">Professionals</a></li>
-      <li><a href="/contacts">Contacts</a></li>
-    </ul>
-  `);
-});
-app.use("/professional", professionalRouter);
-app.use("/contacts", contactsRouter);
+app.use("/", require("./src/routes"));
 
 /* ***********************
  * Log statement to confirm server operation
--- swagger.js
+++ swagger.js
@@ -5,12 +5,13 @@ const doc = {
     title: "My API",
     description: "Description",
   },
-  host: "https://cse341-web-services-1qxq.onrender.com",
-  schemes: ["https", "http"], // Use both https and http for production
+  // host: "https://cse341-web-services-1qxq.onrender.com",
+  host: "localhost:8080", // Use localhost for local development
+  schemes: ["http", "https"], // Use both https and http for production
 };
 
 const outputFile = "./swagger.json";
-const routes = ["./server.js"];
+const routes = ["./src/routes/index.js"];
 
 /* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
 root file where the route starts, such as index.js, app.js, routes.js, etc ... */
